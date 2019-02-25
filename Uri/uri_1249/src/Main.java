335989a9d4a47ded33714ee56a44da1f9150932d
Esengulov Aziz <esengulov00@gmail.com>
2019-02-25 18:02:34 +0600

    solved 1249 uri

--- Uri/uri_1249/src/Main.java
+++ Uri/uri_1249/src/Main.java
@@ -1,15 +1,26 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        String hello = "hello";
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-        char[] letters = hello.toCharArray();
+        while (sc.hasNextLine()) {
+            String input = sc.nextLine();
 
-        for (int i = 0; i < letters.length; i++) {
+            if (input.equals("")) {
+                break;
+            }
+
+            for (int i = 0; i < input.length(); i++) {
+                char c = input.charAt(i);
+                if (c >= 'a' && c <= 'm') c += 13;
+                else if (c >= 'A' && c <= 'M') c += 13;
+                else if (c >= 'n' && c <= 'z') c -= 13;
+                else if (c >= 'N' && c <= 'Z') c -= 13;
+                System.out.print(c);
+            }
+            System.out.println();
         }
     }
 }
+
